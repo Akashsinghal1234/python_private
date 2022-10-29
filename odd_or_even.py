f0360b604658f3d6fd13e77bb25c90b7c4f57113
Akash Singhal <101013452+Akashsinghal1234@users.noreply.github.com>
2022-10-29 22:05:46 +0530

    first commit

---
+++ odd_or_even.py
@@ -0,0 +1,8 @@
+print("odd or even predicter")
+
+number = int(input("enter your number to check : "))
+
+if number % 2 == 0 :
+    print(f"{number} this number is even ")
+else:
+    print(f"{number} this number is odd ")
