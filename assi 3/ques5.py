67dddb4e6c459e247071c1498427b8193ad74f0d
Akash Singhal <101013452+Akashsinghal1234@users.noreply.github.com>
2022-11-01 21:11:41 +0530

    commit 2

--- assi 3/ques5.py
+++ assi 3/ques5.py
@@ -1,12 +1,15 @@
 mod = 0 
 final = 0 
 for i in range (100, 500):
-    mod = i % 10
-    i = i // 10
-    final = final + mod**3
+    final = 0
+    temp = i
+    while (i > 0):
+        mod = i % 10
+        i = i // 10
+        final = final + mod**3
 
-    if final == i :
-       print(final)
+    if final == temp :
+       print(temp)
 
 
 
---
+++ assi 3/ques7.py
@@ -0,0 +1,6 @@
+for i in range (0,5):
+    for j in range (0,5):
+        print(j, end="")
+        j = j - 1
+    print(i)
+
