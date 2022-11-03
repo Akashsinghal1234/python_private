77162a24a56d8224039d806bbc46ee3f28e34dc2
Akash Singhal <101013452+Akashsinghal1234@users.noreply.github.com>
2022-11-03 18:32:21 +0530

    codes

---
+++ assi 3/list.py
@@ -0,0 +1,12 @@
+str = "aaaaiiiiil"
+
+count = 0
+vowel = list("aeiouAEIOU")
+print(vowel)
+'''for alphabet in str:
+    if alphabet in vowel:
+        count = count + 1
+      
+print(f"no of vowels :{count}")'''
+
+
